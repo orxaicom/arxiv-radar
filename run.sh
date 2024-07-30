4b386ec61876ffefe4d119dee5c65576dcf7a164
ORX AI <131006215+orxai@users.noreply.github.com>
2024-07-30 09:42:23 +0330

    Remove first two blank lines of m4

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ unzip daily-arxiv-embeddings.zip
 rm -rf ./output
 mkdir ./output
 m4 app_m4.js > ./app.js
-sed -i '1d' ./app.js
+sed -i '1,2d' ./app.js
 rm app_m4.js
 cp ./index.html ./style.css ./app.js output
 python umap_data.py
