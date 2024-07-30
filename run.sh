0126b1a4473ffd0ac08856dce610665a7f3b82ac
ORX AI <131006215+orxai@users.noreply.github.com>
2024-07-30 09:03:16 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,5 +10,7 @@ kaggle datasets download -d orxaicom/daily-arxiv-embeddings
 unzip daily-arxiv-embeddings.zip
 rm -rf ./output
 mkdir ./output
+m4 app_m4.js > ./app.js
+rm app_m4.js
 cp ./index.html ./style.css ./app.js output
 python umap_data.py
