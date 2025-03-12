c05bf7ecada1bf8c676e749e3d789347a0a50e34
ORX AI <131006215+orxai@users.noreply.github.com>
2025-03-12 18:56:22 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -e
 export KAGGLE_USERNAME=$1
 export KAGGLE_KEY=$2
 
-pip install -U kaggle==1.6.17 numpy umap-learn
+pip install -U kaggle numpy umap-learn
 kaggle datasets download -d orxaicom/daily-arxiv-embeddings
 unzip daily-arxiv-embeddings.zip
 rm -rf ./output
